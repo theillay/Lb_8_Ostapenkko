7079cc430ff1bb373a8bedbddf412ab53ee7136b
theillay <132663973+theillay@users.noreply.github.com>
2023-11-30 19:01:21 +0200

    Add files via upload

---
+++ lb10_1/lb10/lb10.cpp
@@ -0,0 +1,21 @@
+﻿//Обчислити довжину кола і площу круга за заданим діаметром.
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    double diameter;
+
+    cout << "Введіть діаметр кола: ";
+    cin >> diameter;
+    double pi = 3.14159;
+    double perimeter = pi * diameter;
+    double area = pi * diameter * diameter / 4;
+
+    cout << "Довжина кола: " << perimeter << endl;
+    cout << "Площа круга: " << area << endl;
+
+    return 0;
+}
+
